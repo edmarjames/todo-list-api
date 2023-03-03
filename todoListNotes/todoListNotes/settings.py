48c3f5e057be25c199b813975b63d45ff05935b7
edmar.james <edmarjamesbautista@gmail.com>
2023-03-03 16:43:06 +0800

    Modify database settings and generate new requirements.txt also set debug to False in settings.py

--- todoListNotes/todoListNotes/settings.py
+++ todoListNotes/todoListNotes/settings.py
@@ -16,8 +16,8 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-# import pytz for timezone
 import pytz
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,11 +85,15 @@ WSGI_APPLICATION = 'todoListNotes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 

b5b1562366940c46828f05c837197bfc5ead70af
edmar.james <edmarjamesbautista@gmail.com>
2023-03-04 17:09:52 +0800

    run python manage.py collecstatic --noinput to get static files

--- todoListNotes/todoListNotes/settings.py
+++ todoListNotes/todoListNotes/settings.py
@@ -137,8 +137,12 @@ DATETIME_FORMAT = f'{DATE_FORMAT} {TIME_FORMAT}'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 # MEDIA_URL = 'media/'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
